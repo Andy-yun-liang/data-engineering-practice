9e470d0a22a871b8b91c0cc50a06c43eef732c84
Andy Liang <andy.yun.liang@gmail.com>
2024-05-04 01:57:50 -0700

    finished async version of api request, still need to build dockerfile

--- Exercises/Exercise-1/async_main.py
+++ Exercises/Exercise-1/async_main.py
@@ -1,6 +1,9 @@
 import os
 import aiohttp
 import asyncio
+import zipfile
+
+
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -19,18 +22,46 @@ async def downloading_uris(download_uris):
                 if response.status == 200:
                     filepath = f"downloads/{os.path.basename(uri)}"
                     with open(filepath, "wb") as file:
-                        print(f"Downloading zip file: {os.path.basename(uri)}")
+                        print(f"Downloaded {os.path.basename(uri)}")
                         file.write(await response.read())
                 else:
-                    print(f"Zip file: {os.path.basename(uri)} cannot be downloaded")
+                    print(f"Download failed: {os.path.basename(uri)}")
 
-async def unZippingFiles():
+
+
+async def unzip_files():
+    for root, dirs, files in os.walk("downloads"):
+        for file in files:
+            if file.endswith(".zip"):
+                filepath = os.path.join(root, file)
+                with zipfile.ZipFile(filepath, 'r') as ref:
+                    ref.extractall(root)
+                print(f"Extracted {file} into csv")
+
+
+async def deleting_zipped_version():
+    for root, dirs,files, in os.walk("downloads"):
+        for file in files:
+            if file.endswith("zip"):
+                filepath = os.path.join(root,file)
+                os.remove(filepath)
+                print(f"Deleted {file}")
+    
+
+async def processing_data():
     await downloading_uris(download_uris)
+    await unzip_files()
+    await deleting_zipped_version()
+
+
 
 async def main():
     if not os.path.exists("downloads"):
         os.makedirs("downloads")
-    await unZippingFiles()
+    await processing_data()
+
+
 
 if __name__ == "__main__":
     asyncio.run(main())
+    print("Process is done!")
